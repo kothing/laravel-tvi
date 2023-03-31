61cbf331d47ad67f325ac492d6f2d11539bf9d81
Lyove <lyove@outlook.com>
2023-03-31 13:06:32 +0800

    Update tvi.php

--- config/tvi.php
+++ config/tvi.php
@@ -17,6 +17,6 @@ return [
 
     'web' => [
         // If web is disabled, users will always be redirected to admin when trying to access the home page
-        'enabled' => env('TVI_WEB_ENABLED', false)
+        'enabled' => env('TVI_WEB_ENABLED', true)
     ],
 ];
